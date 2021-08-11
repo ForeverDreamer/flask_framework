811abf347f6977f83929451da110861480c805e1
doer <499361328@qq.com>
2021-08-11 18:34:28 +0800

    例行备份

--- click_demo.py
+++ click_demo.py
@@ -1,21 +1,64 @@
+import sys
+import os
+import inspect
+from pprint import pprint as pp
+
+import dotenv
 import click
 
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
-
-
-if __name__ == '__main__':
-    hello()
+# @click.command()
+# @click.option('--count', default=1, help='Number of greetings.')
+# @click.option('--name', prompt='Your name',
+#               help='The person to greet.')
+# def hello(count, name):
+#     """Simple program that greets NAME for a total of COUNT times."""
+#     for x in range(count):
+#         click.echo(f"Hello {name}!")
+#
+#
+# if __name__ == '__main__':
+#     hello()
 
 
 # 使用方式
 # python click_demo.py
 # python click_demo.py --count=3
 # python click_demo.py --help
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def checkenv(*args, **kwargs):
+    click.echo('checkenv')
+    # click.echo(sys.argv[1:])
+    # pp(list(os.environ.items()))
+    sig = inspect.signature(checkenv)
+    print(sig)
+
+
+@click.command()
+def dropdb():
+    click.echo('Dropped the database')
+
+
+@click.command()
+def loaddotenv():
+    click.echo('loaddotenv')
+    pp(dotenv.load_dotenv())
+    gcp_project_id = os.getenv('GCP_PROJECT_ID')
+    service_account_file = os.getenv('SERVICE_ACCOUNT_FILE')
+    storage_bucket_name = os.getenv('STORAGE_BUCKET_NAME')
+    print(gcp_project_id, service_account_file, storage_bucket_name)
+
+
+cli.add_command(checkenv)
+cli.add_command(dropdb)
+cli.add_command(loaddotenv)
+
+if __name__ == '__main__':
+    cli()
--- itsdangerous_demo.py
+++ itsdangerous_demo.py
@@ -1,5 +1,6 @@
 import os
 from itsdangerous.serializer import Serializer
+
 SECRET_KEY = 'base64 encoded random bytes'
 s = Serializer(SECRET_KEY)
 print(dir(s))
